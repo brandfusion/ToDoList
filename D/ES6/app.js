9e1699c42bb5451aa093e10d1d0687060032599d
Daniel Voicu <daniel.voicu@brandfusion.ro>
2019-02-01 14:40:52 +0200

    Update app.js

--- D/ES6/app.js
+++ D/ES6/app.js
@@ -1,11 +1,11 @@
 const data = []
 const renderData = (data) => {
-  return document.getElementById("app").innerHTML = `
+  document.getElementById("app").innerHTML = `
     <div id="list">
       <div class="item">Item1</div>      
       <div class="item">Item2</div>      
     </div>
-    <div> id="actions">
+    <div id="actions">
       <button type="button">Delete checked</button>
       <button type="button">Mark all as checked</button>
     </div>
